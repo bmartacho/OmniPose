854cc240c2ee5b5db01964fa71409901dbfbba38
Bruno Artacho <44781803+bmartacho@users.noreply.github.com>
2021-03-03 15:18:34 -0500

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -9,10 +9,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import models.pose_resnet
+import models.omnipose
 import models.pose_hrnet
-import models.hourglass
