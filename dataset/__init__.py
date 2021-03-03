4b600674efe129f277b8121479f5e67b5f56ffbc
Bruno Artacho <44781803+bmartacho@users.noreply.github.com>
2021-03-03 15:39:11 -0500

    Update __init__.py

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,8 +1,10 @@
-# ------------------------------------------------------------------------------
-# Copyright (c) Microsoft
-# Licensed under the MIT License.
-# Written by Bin Xiao (Bin.Xiao@microsoft.com)
-# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------ #
+# ------------------------------------------------------------------------------ #
+#                                    OmniPose                                    #
+#      Rochester Institute of Technology - Vision and Image Processing Lab       #
+#                      Bruno Artacho (bmartacho@mail.rit.edu)                    #
+# ------------------------------------------------------------------------------ #
+# ------------------------------------------------------------------------------ #
 
 from __future__ import absolute_import
 from __future__ import division
@@ -10,4 +12,3 @@ from __future__ import print_function
 
 from .mpii import MPIIDataset as mpii
 from .coco import COCODataset as coco
-from .posetrack import PoseTrackDataset as posetrack
