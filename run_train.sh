3e0e597e15695fcc8193d0abd794f57d94b84a56
Bruno Artacho <44781803+bmartacho@users.noreply.github.com>
2021-03-03 14:50:35 -0500

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -1,12 +1,18 @@
+# ========================================= #
+# Choose one of the following Config files:
+# ========================================= #
+
+# ========================================= #
+#                MPII Dataset
+# ========================================= #
 # CFG='experiments/mpii/hrnet/w48_256x256_adam_lr1e-3.yaml'
 
+# ========================================= #
+#                COCO Dataset
+# ========================================= #
 # CFG='experiments/coco/hrnet/w48_128x96_adam_lr1e-3.yaml'
 # CFG='experiments/coco/hrnet/w48_256x192_adam_lr1e-3.yaml'
 CFG='experiments/coco/hrnet/w48_384x288_adam_lr1e-3.yaml'
-# CFG='experiments/coco/hrnet/w48_512x384_adam_lr1e-3.yaml'
-# CFG='experiments/coco/hrnet/w48_640x640_adam_lr1e-3.yaml'
-
-# CFG='experiments/posetrack/omnipose_384x288.yaml'
 
 OPTS=None
 MODELDIR=''
